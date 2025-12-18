647cfee6d7aae518cd13b14bf516192cc0ed4d8b
Stephan Ewen <stephan@restate.dev>
2025-12-18 15:13:49 +0100

    Add stacked concurrency example for semaphore

--- typescript/patterns-use-cases/src/semaphore/service.ts
+++ typescript/patterns-use-cases/src/semaphore/service.ts
@@ -1,26 +1,53 @@
-import { Context, service } from "@restatedev/restate-sdk";
+import * as restate from "@restatedev/restate-sdk";
+import { serde } from "@restatedev/restate-sdk-zod";
+import { z } from "zod";
 import { limitHandler } from "./semaphore_client";
 
-export const myService = service({
+const Request = z.object({
+  team: z.string(),
+  user: z.string(),
+  agentId: z.string(),
+  prompt: z.string()
+});
+type Request = z.infer<typeof Request>;
+
+
+export const myService = restate.service({
   name: "myService",
   handlers: {
-    expensiveMethod: limitHandler(
+    expensiveMethod: restate.createServiceHandler(
       {
-        id: "myService/expensiveMethod",
-        concurrency: [
-          {
-            scope: "handler",
-            key: (input) => `left:${input.left}`,
-            limit: 2,
-          }
-        ],
+        input: serde.zod(Request),
+        output: serde.zod(z.void())
       },
-      async (ctx: Context, params: { left: number; right: number }) => {
-        // very expensive - important that the semaphore protects this
-        await ctx.sleep({ days: 1});
-      }
-    ),
-  },
+      limitHandler(
+        {
+          id: "myService/expensiveMethod",
+          concurrency: [
+            {
+              scope: "handler",
+              key: (input) => input.agentId,
+              limit: 2,
+            },
+            {
+              scope: "handler",
+              key: (input) => input.user,
+              limit: 5,
+            },
+            {
+              scope: "handler",
+              key: (input) => input.team,
+              limit: 100,
+            }
+          ],
+        },
+        async (ctx: restate.Context, request) => {
+          // very expensive - important that the semaphore protects this
+          await ctx.sleep({ days: 1});
+        }
+      )
+    )
+  }
 });
 
 export type MyService = typeof myService;
